023c470c4ef2456aa378f5545e2bf49faaad9b58
Bzzigs <151874182+Bzzigs@users.noreply.github.com>
2025-03-01 18:40:05 +0300

    first chagne

--- donate.js
+++
@@ -1,6 +0,0 @@
-let phonenumber;
-
-document.getElementById('don').onclick = function() {
-    phonenumber = document.getElementById('phonenumb').value;
-    alert("Phone number captured: " + phonenumber);
-};
